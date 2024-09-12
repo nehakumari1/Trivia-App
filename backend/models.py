c481ff45f88127f28db80480582c8b3855ab0d71
Neha <nehak5139@gmail.com>
2024-09-12 23:14:00 +0530

    models.py

--- backend/models.py
+++ backend/models.py
@@ -3,88 +3,88 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+# Database configurations using environment variables
 DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
 DB_USER = os.getenv('DB_USER', 'manal')
 DB_PASSWORD = os.getenv('DB_PASSWORD', '123456m')
 DB_NAME = os.getenv('DB_NAME', 'trivia')
-database_path = "postgres://{}:{}@{}/{}".format(
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+db_url = f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 db = SQLAlchemy()
 
 '''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
+initialize_db(app)
+    This function links a Flask app to the SQLAlchemy service
 '''
 
-
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def initialize_db(app, db_url=db_url):
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-
 '''
-Question
-
+QuizItem
+    Represents the questions in the quiz game
 '''
 
+class QuizItem(db.Model):
+    __tablename__ = 'quiz_items'
 
-class Question(db.Model):
-    __tablename__ = 'questions'
+    item_id = Column(Integer, primary_key=True)
+    question_text = Column(String)
+    correct_response = Column(String)
+    category_type = Column(String)
+    difficulty_level = Column(Integer)
 
-    id = Column(Integer, primary_key=True)
-    question = Column(String)
-    answer = Column(String)
-    category = Column(String)
-    difficulty = Column(Integer)
+    def __init__(self, question_text, correct_response, category_type, difficulty_level):
+        self.question_text = question_text
+        self.correct_response = correct_response
+        self.category_type = category_type
+        self.difficulty_level = difficulty_level
 
-    def __init__(self, question, answer, category, difficulty):
-        self.question = question
-        self.answer = answer
-        self.category = category
-        self.difficulty = difficulty
-
-    def insert(self):
+    # Method to insert a new quiz item into the database
+    def save(self):
         db.session.add(self)
         db.session.commit()
 
-    def update(self):
+    # Method to update the existing quiz item details
+    def apply_update(self):
         db.session.commit()
 
-    def delete(self):
+    # Method to delete the quiz item from the database
+    def remove(self):
         db.session.delete(self)
         db.session.commit()
 
-    def format(self):
+    # Format the quiz item data for easier consumption in APIs
+    def serialize(self):
         return {
-            'id': self.id,
-            'question': self.question,
-            'answer': self.answer,
-            'category': self.category,
-            'difficulty': self.difficulty
+            'item_id': self.item_id,
+            'question_text': self.question_text,
+            'correct_response': self.correct_response,
+            'category_type': self.category_type,
+            'difficulty_level': self.difficulty_level
         }
 
-
 '''
-Category
-
+QuizCategory
+    Represents the category of quiz questions
 '''
 
+class QuizCategory(db.Model):
+    __tablename__ = 'quiz_categories'
 
-class Category(db.Model):
-    __tablename__ = 'categories'
+    category_id = Column(Integer, primary_key=True)
+    category_name = Column(String)
 
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
+    def __init__(self, category_name):
+        self.category_name = category_name
 
-    def __init__(self, type):
-        self.type = type
-
-    def format(self):
+    # Format the quiz category data for easier consumption in APIs
+    def serialize(self):
         return {
-            'id': self.id,
-            'type': self.type
+            'category_id': self.category_id,
+            'category_name': self.category_name
         }
